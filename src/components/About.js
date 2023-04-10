42eca3327ef3bc3c23ca2b4bb0a14e1b661a7412
grantferowich <g.ferowich@gmail.com>
2023-04-10 12:24:46 -0500

    successfully appended more content

---
+++ src/components/About.js
@@ -0,0 +1,94 @@
+import React from 'react'
+/* import './About.css'; */
+
+
+
+export default function About() {
+  return (
+    <div className='about-container'>
+        <h1>Wisdom Party</h1>
+        <h2>Principles</h2>
+        <br></br>
+        <details>
+        <summary>1. Limited Powers Principle</summary>
+            <p>That the federal government is one of limited powers, derived solely from the constitution, and the grants of power shown therein, ought to be strictly construed by all the departments and agents of the government, and that it is inexpedient and dangerous to exercise doubtful constitutional powers.</p>
+        </details>
+        <details>
+            <summary>2. Restraint Principle</summary>
+            <p>That the constitution does not confer upon the general government the power to commence and carry on, a general system of internal improvements.</p>
+        </details>
+        <details>
+            <summary>3. Non-Assumption of Debts Principle</summary>
+            <p>That the constitution does not confer authority upon the federal government, directly or indirectly, to assume the debts of the several states, contracted for local internal improvements, or other state purposes; nor would such assumption be just or expedient.</p>
+        </details>
+        <details>
+            <summary>4. Protection of Competition Principle</summary>
+            <p>That justice and sound policy forbid the federal government to foster one branch of industry to the detriment of another, or to cherish the interests of one portion to the injury of another portion of our common country—that every citizen and every section of the country, has a right to demand and insist upon an equality of rights and privileges, and to complete and ample protection of person and property from domestic violence, or foreign aggression.</p>
+        </details>
+        <details>
+            <summary>5. Rigid Economy Principle</summary>
+            <p>That it is the duty of every branch of the government, to enforce and practice the most rigid economy, in conducting our public affairs, and that no more revenue ought to be raised, than is required to defray the necessary expenses of the government.</p>
+        </details>
+        <details>
+            <summary>6. Non-Central Bank Principle</summary>
+            <p>That congress has no power to charter a national bank; that we believe such an institution one of deadly hostility to the best interests of the country, dangerous to our republican institutions and the liberties of the people, and calculated to place the business of the country within the control of a concentrated money power, and above the laws and the will of the people.</p>
+        </details>
+        <details>
+            <summary>7. Separation Of Government And Banks Principle</summary>
+            <p>That the separation of the moneys of the government from banking institutions, is indispensable for the safety of the funds of the government, and the rights of the people.</p>
+        </details>
+        <details>
+            <summary>8. The Thomas Jefferson Principle</summary>
+            <p>That the liberal principles embodied by Jefferson in the Declaration of Independence, and sanctioned in the constitution, which makes ours the land of liberty, and the asylum of the oppressed of every nation, have ever been cardinal principles in the democratic faith; and every attempt to abridge the present privilege of becoming citizens, and the owners of soil among us, ought to be resisted with the same spirit which swept the alien and sedition laws from our statute-book.</p>
+        </details>
+        <details>
+            <summary>9. Voter ID Principle</summary>
+            <p>You have to show an ID to buy beer, therefore, you have to show an ID to vote.</p>
+        </details>
+        <details>
+            <summary>10. The Deliberate Defense Principle</summary>
+            <p> 48 out of 50 state legislatures must ratify any initiation of a military operation or declaration of war.</p>
+        </details>
+        <details>
+            <summary>11. Fight Smart, Obvious, and Winnable Wars Principle</summary>
+            <p>Only fight in wars which are smart, and winnable. Do not initiate military action in the pursuit of amorphous ends, such as patrol or the training of a police force in a foreign country. The United States Defense Department must only be tasked with fighting winnable wars with clear, obvious, tangible end outcomes.</p>
+        </details>
+        <details>
+            <summary>12. States As Laboratories For Policy Principle</summary>
+            <p>Act locally, wonder universally. Examine policies by asking, "Does this policy scale?" It is okay if the answer is no. If the answer is yes, can you extract and release a blueprint for others to replicate your successes? <em>"It is one of the happy incidents of the federal system that a single courageous State may, if its citizens choose, serve as a laboratory; and try novel social and economic experiments without risk to the rest of the country."</em> - Justice Louis Brandeis, <em>New State Ice Co. v. Liebermann</em>, (1932).</p>
+        </details>
+        <details>
+            <summary>13. Accountability Principle</summary>
+            <p>States have to create agencies for prosecuting crimes committed by federal agents which violate the rights of individuals.</p>
+        </details>
+        <details>
+            <summary>14. Remove The Federal Income Tax Principle</summary>
+            <p>The Federal Income Tax is immoral. The federal government must not be complicit in unethical practices against the very free people which endow the government with the sacred responsibility of serving as their agent.</p>
+            <p>The Federal Income Tax is not grounded in moral thinking. If you wake up at the crack of dawn to improve at your craft and enhance your work, you are not inherently incurring an obligation to pay the federal government anything. Diligence is something praiseworthy. Diligence is not something, which, like alcohol or certain substance use, must be subject to a tax which is intended to have the effect of <em>reducing</em> the activity at hand.</p> 
+        </details>
+        <details>
+            <summary>15. Abolition of the Federal Reserve Principle</summary>
+            <p>The Federal Reserve has facilitated a pathway by which the United States has accrued $31 trillion worth of debt. "I sincerely believe with you, that banking establishments are more dangerous than standing armies; & that the principle of spending money to be paid by posterity, under the name of funding, is but swindling futurity on a large scale," Thomas Jefferson wrote in 1816. A central bank, such as the Federal Reserve, is unconstitutional, according to Jefferson. The Federal Reserve Act of 1913 instituted today's central bank, however, the existence of a central bank remains unconstitutional.</p>
+            <p>"Thomas Jefferson was afraid that a national bank would create a financial monopoly that might undermine state banks and adopt policies that favored financiers and merchants, who tended to be creditors, over plantation owners and family farmers, who tended to be debtors," according to Andrew T. Hill, Federal Reserve Bank of Philadelphia. <a href='https://www.federalreservehistory.org/essays/first-bank-of-the-us' target="_blank" rel='noreferrer'>Report</a>.</p>
+            <p>The Federal Reserve's role as a provider of payment rails for the facilitation of commerce between states must be handled by state banks, such as the bank of Illinois, the bank of North Carolina, the bank of Texas, the bank of Florida, and so on.</p>
+        </details>
+        <details>
+            <summary></summary>
+            <p></p>
+        </details>
+        <details>
+            <summary></summary>
+            <p></p>
+        </details>
+        <details>
+            <summary></summary>
+            <p></p>
+        </details>
+        <details>
+            <summary></summary>
+            <p></p>
+        </details>
+        <h3>© Copyright, Grant Ferowich</h3>
+    </div>
+  )
+}
